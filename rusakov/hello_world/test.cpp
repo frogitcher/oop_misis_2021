1c969d3e1664dd4167628746eb1a522a0e1fd29a
ShockLess <fedor.avilov@yandex.ru>
2022-03-14 15:55:10 +0300

    Delete rusakov directory

--- rusakov/hello_world/test.cpp
+++
@@ -1,7 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "helloworld.h"
-
-TEST_CASE("testing the factorial function") {
-    CHECK(HelloWorld() == "Hello World!");
-}
