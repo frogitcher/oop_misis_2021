ea30c863fadbd0711a0154688240c0c033e7f480
rml admin <medjidi@yandex.ru>
2022-04-13 01:43:55 +0300

    Rational, magomedoV

---
+++ magomedov_m_m/rational/rational.cpp
@@ -0,0 +1,132 @@
+#include "rational.h"
+#include <stdexcept>
+#include <iostream>
+
+int gcd(int a, int b) {
+	if (b == 0)return a;
+	else return gcd(b, a % b);
+}
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+Rational::Rational(const Rational& other) {
+	*this = other;
+}
+
+Rational::Rational()
+	:num(0)
+	, den(1)
+{}
+Rational::Rational(const int _num)
+	:num(_num)
+	, den(1)
+{}
+Rational::Rational(const int _num, int _den)
+	: num(_num)
+	, den(_den){
+	Check();
+	Normal();
+}
+
+void Rational::Check() {
+	if (den == 0) {
+		throw std::invalid_argument("You cannot divide by zero");
+	}
+	else if (den < 0) {
+		num = -num;
+		den = -den;
+	}
+}
+void Rational::Normal() {
+	int c = gcd(abs(den), num);
+	if (c > 1) {
+		num /= c;
+		den /= c;
+	}
+}
+
+bool Rational:: operator<(const Rational& rhs) const {
+	return (num * rhs.den < den* rhs.num);
+}
+bool Rational:: operator>(const Rational& rhs) const {
+	return (num * rhs.den > den * rhs.num);
+}
+bool Rational:: operator<=(const Rational& rhs) const {
+	return (num * rhs.den <= den* rhs.num);
+}
+bool Rational:: operator>=(const Rational& rhs) const {
+	return (num * rhs.den >= den * rhs.num);
+}
+bool Rational:: operator==(const Rational& rhs) const {
+	return (num * rhs.den == den* rhs.num);
+}
+bool Rational:: operator!=(const Rational& rhs) const {
+	return (num * rhs.den != den * rhs.num);
+}
+
+
+std::istream& operator>>(std::istream& input, Rational& rhs)
+{	char c;
+	return input >> rhs.num >> c >> rhs.den;
+}
+std::ostream& operator<<(std::ostream& output, Rational& rhs)
+{
+	return output << rhs.num <<"/"<<rhs.den;
+}
+
+
+Rational Rational::operator+(const Rational& rhs) const{
+	return Rational(num * rhs.den + rhs.num * den, den * rhs.den);
+};
+Rational Rational::operator-(const Rational& rhs) const {
+	return Rational(num * rhs.den - rhs.num * den, den * rhs.den);
+};
+Rational Rational::operator*(const Rational& rhs) const {
+	return Rational(num * rhs.num, den * rhs.den);
+};
+Rational Rational::operator/(const Rational& rhs) const {
+	if (rhs.num == 0) {
+		throw std::invalid_argument("You cannot divide by zero");
+	}
+	else {
+		return Rational(num * rhs.den, den * rhs.num);
+	}
+};
+
+
+Rational& Rational::operator+=(const Rational& rhs) {
+	*this = *this+rhs;
+	return *this;
+};
+Rational Rational::operator++(int) {
+	Rational old(*this);
+	num += den;
+	return old;
+};
+Rational Rational::operator--(int) {
+	Rational old(*this);
+	num -= den;
+	return old;
+};
+Rational& Rational::operator++() {
+	num += den;
+	return *this;
+}
+Rational& Rational::operator--() {
+	num -= den;
+	return *this;
+}
+Rational& Rational::operator-=(const Rational& rhs) {
+	*this = *this - rhs;
+	return *this;
+};
+Rational& Rational::operator*=(const Rational& rhs) {
+	*this = *this * rhs;
+	return *this;
+};
+Rational& Rational::operator/=(const Rational& rhs) {
+
+	*this = *this / rhs;
+	return *this;
+};
+
---
+++ magomedov_m_m/rational/test.cpp
@@ -0,0 +1,44 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "rational.h"
+
+TEST_CASE("Booleans") {
+    Rational a(1, 2), b(-3, 4), c, d(1), f(5, 10);
+    CHECK((a != b));
+    CHECK((a == f));
+    CHECK((a >= b));
+    CHECK((c <= d));
+    CHECK(!(a > f));
+    CHECK((b < d));
+}
+
+TEST_CASE("+ - * / += *= -= /=") {
+    Rational a(1, 2), b(-3, 4), c, d(1), f(5, 10);
+    CHECK((a+b == Rational(-1,4)));
+    CHECK((a-b == Rational(5,4)));
+    CHECK((a * d==a));
+    CHECK((f / a==Rational(1)));
+    a += b;
+    c -= d;
+    f *= a;
+    b /= d;
+    CHECK((a == Rational(-1, 4)));
+    CHECK((c == Rational(-1)));
+    CHECK((f == Rational(-1,8)));
+    CHECK((b == Rational(-3,4)));
+}
+
+TEST_CASE("++ --") {
+    Rational a(1, 2), b(-3, 4), c, d(1), f(5, 10);
+    CHECK(!(Rational (1,2)== ++a));
+    CHECK((Rational (-3,4) == b++));
+    CHECK((Rational () == --d));
+    CHECK((Rational (5,10)==  f--));
+}
+
+TEST_CASE("Errors") {
+    Rational a(0,1);
+    CHECK_THROWS_WITH(Rational(1, 0), "You cannot divide by zero");
+    CHECK_THROWS_WITH(Rational(1) /= a, "You cannot divide by zero");
+}
+ 
