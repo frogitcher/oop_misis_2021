cf462ee93c8d70607519cc737569c0558816e68a
medjidi <medjidi@yandex.ru>
2022-04-13 13:08:47 +0300

    Update rational.cpp

--- magomedov_m_m/rational/rational.cpp
+++ magomedov_m_m/rational/rational.cpp
@@ -6,9 +6,6 @@ int gcd(int a, int b) {
 	if (b == 0)return a;
 	else return gcd(b, a % b);
 }
-int lcm(int a, int b) {
-	return a * b / gcd(a, b);
-}
 Rational::Rational(const Rational& other) {
 	*this = other;
 }
