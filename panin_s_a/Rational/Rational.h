269290b80f519f32eb2e491d56c82dd379bd7dba
Sergey <planse2221@gmail.com>
2022-04-21 22:58:36 +0300

    Rational numbers

---
+++ panin_s_a/Rational/Rational.h
@@ -0,0 +1,44 @@
+#pragma once
+
+class Rational {
+public:
+    Rational();
+    Rational(int _num);
+    Rational(int _num, int _den);
+    Rational(const Rational& other);
+    ~Rational() = default;
+
+    bool operator<(const Rational& rhs) const;
+    bool operator<=(const Rational& rhs) const;
+    bool operator>(const Rational& rhs) const;
+    bool operator>=(const Rational& rhs) const;
+    bool operator==(const Rational& rhs) const;
+    bool operator!=(const Rational& rhs) const;
+
+    Rational operator+(const Rational& rhs) const;
+    Rational operator-(const Rational& rhs) const;
+    Rational operator/(const Rational& rhs) const;
+    Rational operator*(const Rational& rhs) const;
+
+    Rational operator+=(const Rational& rhs);
+    Rational operator-=(const Rational& rhs);
+    Rational operator*=(const Rational& rhs);
+    Rational operator/=(const Rational& rhs);
+
+    Rational operator-();
+    Rational operator+();
+
+    Rational& operator++();
+    Rational operator++(int);
+    Rational& operator--();
+    Rational operator--(int);
+
+    int GetDenominator() const;
+    int GetNumerator() const;
+    void SetDenominator(int val);
+    void SetNumerator(int val);
+
+private:
+    int num, den;
+    void Normalize();
+};
