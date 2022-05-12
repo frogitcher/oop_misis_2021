c22b0e24df86db3b57f276fc9b6e9e9eaaf74ebb
Artem216 <artemtsykanov22@gmail.com>
2022-05-12 18:33:19 +0300

    Adding DA

---
+++ Tsykanov_A/dynamic_array/DynamicArray.cpp
@@ -0,0 +1,128 @@
+#include "DynamicArray.h"
+#include <algorithm>
+#include <stdexcept>
+
+// Конструкторы
+DynamicArray::DynamicArray(int64_t lenght, int value = 0) 
+    : size(lenght)
+    , capacity(lenght) {
+    data = new int[lenght];
+    std::fill(begin(), end(), value);
+}
+DynamicArray::DynamicArray(const DynamicArray& other)
+    : size(other.size)
+    , capacity(other.capacity) {
+    data = new int[capacity];
+    std::copy(other.data, other.data + other.size, data);
+}
+DynamicArray::DynamicArray(const std::initializer_list<int>& list)
+    : size(list.size())
+    , capacity(list.size()){
+    data = new int[capacity];
+    std::copy(list.begin(), list.end(), data);
+}
+DynamicArray::~DynamicArray()
+{
+    delete[] data;
+}
+//Методы
+void DynamicArray::resize(int64_t new_size, int value = 0){
+    if (new_size < 0) {
+        throw std::length_error("Length can't be negative!");
+    }
+    if (new_size >= capacity){
+        reallocate(new_size);
+    } 
+    std::fill(begin() + ((size <= new_size) ? size : 0), begin() + new_size, value);
+    size = new_size;
+};
+void DynamicArray::push_back(int value){
+    if (size == capacity){
+        if (capacity == 0){
+            capacity = 1;
+        }
+        reallocate(size);
+    }
+    data[size++] = value;
+};
+void DynamicArray::pop_back(){
+    if (size == 0) {
+        throw std::length_error("Array is empty!");
+    }
+    --size;  
+};
+void DynamicArray::erase(int64_t index){
+    if (index < 0 || index >= size) {
+        throw std::out_of_range("Index is out of range!");
+    }
+    if (size == 0) {
+        throw std::length_error("Array is empty!");
+    }
+    for (int64_t i = index; i < size - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+    --size;
+};
+void DynamicArray::assign(int64_t new_size, int value){
+    if (new_size < 0) {
+        throw std::length_error("Length can't be negative!");
+    }
+    if (new_size == 0) {
+        size = 0;
+        return;
+    }
+    reallocate(new_size);
+    for(int64_t i = 0; i < new_size; i++){
+        data[i] = value;
+    }
+    size = new_size;
+};
+void DynamicArray::swap(DynamicArray& other){
+    std::swap(size, other.size);
+    std::swap(capacity, other.capacity);
+    std::swap(data, other.data);
+};
+void DynamicArray::insert(int64_t index, int value){
+    if (size == capacity){
+        if (capacity == 0){
+            capacity = 1;
+        }
+        reallocate(size);
+    }
+    ++size;
+    for(int64_t i = index + 1; i < size; i++){
+        data[i] = data[i - 1];
+    }
+    data[index] = value;
+};
+//операторы
+int& DynamicArray::operator[](int64_t i) const{
+    return *(data + i);
+};
+int& DynamicArray::at(int64_t i) const{
+    if (i < 0 || i >= size) {
+        throw std::out_of_range("Index is out of range!");
+    }
+    return *(data + i);    
+}; 
+bool DynamicArray::operator==(const DynamicArray& other) const{
+    return std::equal<int*, int*>(data, data + size, other.data);
+};
+bool DynamicArray::operator!=(const DynamicArray& other) const{
+    return !(*this == other);
+};
+DynamicArray& DynamicArray::operator=(const DynamicArray& rhs){
+    size = rhs.size;
+    capacity = rhs.capacity;
+    std::copy(rhs.data, rhs.data + rhs.size, data);
+    return *this;
+};
+void DynamicArray::reallocate(int64_t new_size) {
+    int new_capacity = std::max(new_size, size * 2);
+        int *new_data = new int[new_capacity];
+        std::copy(begin(), end(), new_data);
+        delete[] data;
+        data = new_data;
+        capacity = new_capacity;
+};
+
---
+++ Tsykanov_A/dynamic_array/DynamicArray.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <initializer_list>
+#include <cstdint>
+class DynamicArray
+{
+public:
+    //Конструкторы
+    DynamicArray() = default;
+    DynamicArray(int64_t lenght, int value);
+    DynamicArray(const DynamicArray& other);
+    DynamicArray(const std::initializer_list<int>& list);
+    ~DynamicArray();
+    //Методы
+    void push_back(int value);
+    inline void clear(){size = 0;};
+    void pop_back();
+    void erase(int64_t index);
+    void resize(int64_t new_size, int value);
+    void assign(int64_t new_size, int value);
+    void insert(int64_t index, int value);
+    void swap(DynamicArray& other);
+    //указатели
+    inline int* begin(){return data;};
+    inline int* end(){return data + size;};
+    // операторы
+    DynamicArray& operator=(const DynamicArray& rhs);
+    int& operator[](int64_t i) const;
+    int& at(int64_t i) const;
+    bool operator==(const DynamicArray& other) const;
+    bool operator!=(const DynamicArray& other) const;
+    inline int64_t Size() const {return size;};
+    inline int64_t Capacity() const{return capacity;};
+    inline bool Empty() const { return size == 0;}
+    inline int* GetData() const { return data;}
+    inline friend void Reallocate(DynamicArray& _da, int64_t new_size) {
+            _da.reallocate(new_size);
+        }
+private:
+    int64_t size = 0;
+    int64_t capacity = 0;
+    int* data;
+    void reallocate(int64_t new_capacity);
+};
+
+inline void Reallocate(DynamicArray& _da, int64_t new_size);
