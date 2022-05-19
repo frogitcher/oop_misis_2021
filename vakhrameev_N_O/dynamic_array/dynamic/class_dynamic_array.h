1941ac98036920d6d613d8c4909c6507cd6788ae
hekaido <91501692+hekaido@users.noreply.github.com>
2022-05-20 00:28:44 +0300

    Update class_dynamic_array.h

--- vakhrameev_N_O/dynamic_array/dynamic/class_dynamic_array.h
+++ vakhrameev_N_O/dynamic_array/dynamic/class_dynamic_array.h
@@ -2,15 +2,17 @@
 #include <initializer_list>
 #include <stddef.h>
 #include <string>
+#include <iostream>
 class Dynamic_array
 {
 public:
-	Dynamic_array(size_t n=0, int value=0);
+	Dynamic_array() = default;
+	Dynamic_array(size_t n, int value=0);
 	Dynamic_array(const Dynamic_array& other);
 	Dynamic_array(const std::initializer_list<int>& other);
 	~Dynamic_array();
 
-	Dynamic_array& operator=(const Dynamic_array& other);
+	
 	size_t Capacity() const;
 	size_t Size() const;
 	bool Empty() const;
@@ -23,6 +25,9 @@ public:
 	int* begin() const;
 	int* end() const;
 	void swap(Dynamic_array& other);
+	void resize(size_t new_size);
+	void reallocate(size_t new_capacity);
+	Dynamic_array& operator=(const Dynamic_array& other);
 	bool operator==(const Dynamic_array& other)const;
 	bool operator!=(const Dynamic_array& other)const;
 	bool operator>=(const Dynamic_array& other)const;
@@ -30,8 +35,10 @@ public:
 	bool operator>(const Dynamic_array& other)const;
 	bool operator<(const Dynamic_array& other)const;
 
+
 private:
 	size_t size = 0;
 	size_t capacity = 0;
 	int* data;
 };
+std::ostream& operator<<(std::ostream &os,Dynamic_array &other);
