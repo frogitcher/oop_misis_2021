aac52c9bcc272e3b2ea9a6bf7aa8fd91d5b74860
hekaido <91501692+hekaido@users.noreply.github.com>
2022-05-19 23:38:18 +0300

    Update class_dynamic_array.h

--- vakhrameev_N_O/dynamic_array/dynamic/class_dynamic_array.h
+++ vakhrameev_N_O/dynamic_array/dynamic/class_dynamic_array.h
@@ -5,13 +5,12 @@
 class Dynamic_array
 {
 public:
-	Dynamic_array() = default;
-	Dynamic_array(size_t n, int value=0);
+	Dynamic_array(size_t n=0, int value=0);
 	Dynamic_array(const Dynamic_array& other);
 	Dynamic_array(const std::initializer_list<int>& other);
 	~Dynamic_array();
 
-	Dynamic_array operator=(const Dynamic_array& other);
+	Dynamic_array& operator=(const Dynamic_array& other);
 	size_t Capacity() const;
 	size_t Size() const;
 	bool Empty() const;
@@ -21,8 +20,8 @@ public:
 	void clear();
 	void assign(size_t n, int value=0);
 	void erase(size_t index);
-	int* begin();
-	int* end();
+	int* begin() const;
+	int* end() const;
 	void swap(Dynamic_array& other);
 	bool operator==(const Dynamic_array& other)const;
 	bool operator!=(const Dynamic_array& other)const;
