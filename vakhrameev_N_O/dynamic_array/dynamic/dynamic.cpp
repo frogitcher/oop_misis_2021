00b62f6b4a472f83f6aedea5f7f3a1acbb7e0165
hekaido <91501692+hekaido@users.noreply.github.com>
2022-05-20 00:27:53 +0300

    Update dynamic.cpp

--- vakhrameev_N_O/dynamic_array/dynamic/dynamic.cpp
+++ vakhrameev_N_O/dynamic_array/dynamic/dynamic.cpp
@@ -4,109 +4,126 @@
 #include <stdexcept>
 Dynamic_array::Dynamic_array(size_t n, int value)
 {
-	size = n;
-	capacity = n*2;
-	data = new int[n];
-	std:: fill(data,data+size,value);
+    size = n;
+    capacity = n * 2;
+    data = new int[capacity];
+    std::fill(data, data + size, value);
 }
 
 Dynamic_array::Dynamic_array(const Dynamic_array& other)
 {
-	size = other.size;
-	capacity = other.size;
-	data = other.data;
+    size = other.size;
+    capacity = other.size;
+    int* new_data=new int[capacity];
+    std::copy(other.data, other.data+size, new_data);
+    data=new_data;
 }
 
 Dynamic_array::~Dynamic_array() {
-	delete[] data;
+    delete[] data;
 }
 Dynamic_array::Dynamic_array(const std::initializer_list<int>& other)
 {
-	size = other.size();
-	capacity = other.size() * 2;
-	data = new int[other.size()*2];
-	std::copy(other.begin(),other.end(),data);
+    size = other.size();
+    capacity = other.size() * 2;
+    data = new int[other.size() * 2];
+    std::copy(other.begin(), other.end(), data);
 }
 
 int& Dynamic_array:: operator[](size_t i)
 {
-	if (i>size)
-    {
-        throw std::out_of_range("index out of range");
-    }
-    else
-    {
-        return *(data+i);
-    }
+    return *(data + i);
 }
 
-size_t Dynamic_array:: Capacity() const
+size_t Dynamic_array::Capacity() const
 {
-	return capacity;
+    return capacity;
 }
 
 size_t Dynamic_array::Size() const
 {
-	return size;
+    return size;
 }
 
 bool Dynamic_array::Empty() const
 {
-	if (size == 0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if (size == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void Dynamic_array::push_back(int value)
 {
-	if (size == capacity && capacity!=0)
-	{
-		capacity *= 2;
-		int* temp_data = new int[capacity];
-		std::copy(temp_data, temp_data + size, data);
-		delete[] data;
-		data = temp_data;
-	}
-	data[size++] = value;
+    resize(++size);
+    data[size - 1] = value;
 }
 
+void Dynamic_array::resize(size_t new_size)
+{
+    if (new_size == 0)
+    {
+        size = new_size;
+        return;
+    }
+    reallocate(new_size);
+    if (size < new_size)
+    {
+        for (int i = size;i < new_size;i++)
+        {
+            data[i] = 0;
+        }
+    }
+    size = new_size;
+
+}
+void Dynamic_array::reallocate(size_t new_capacity)
+{
+    if (new_capacity > capacity)
+    {
+        capacity = new_capacity;
+        int* new_data = new int[new_capacity];
+        std::copy(data, data + capacity, new_data);
+        delete[] data;
+        data = new_data;
+    }
+}
 void Dynamic_array::pop_back()
 {
-	if (size != 0)
-	{
-		data[--size] = 0;
-	}
-	else
-	{
-		throw std::range_error("Dynamic array has no elements!");
-	}
+    if (size != 0)
+    {
+        data[--size] = 0;
+    }
+    else
+    {
+        throw std::range_error("Dynamic array has no elements!");
+    }
 }
 
 void Dynamic_array::assign(size_t n, int value)
 {
-    size=n;
-    capacity=n*2;
-    data=new int[n*2];
-    std::fill(data,data+n,value);
+    size = n;
+    capacity = n * 2;
+    data = new int[n * 2];
+    std::fill(data, data + n, value);
 }
 void Dynamic_array::clear()
 {
     delete[] data;
-    size=0;
+    size = 0;
 }
 
 void Dynamic_array::erase(size_t i)
 {
-    if (i<size)
+    if (i < size)
     {
-        for (int j=i;j<size-1;j++)
+        for (int j = i;j < size - 1;j++)
         {
-            data[j]=data[j+1];
+            data[j] = data[j + 1];
         }
         size--;
     }
@@ -116,110 +133,125 @@ void Dynamic_array::erase(size_t i)
     }
 }
 
-int* Dynamic_array::begin()
+int* Dynamic_array::begin() const
 {
     return data;
 }
 
-int* Dynamic_array::end()
+int* Dynamic_array::end() const
 {
-    return data+size;
+    return data + size;
 }
 
 void Dynamic_array::swap(Dynamic_array& other)
 {
-    std::swap(size,other.size);
-    std::swap(capacity,other.capacity);
-    std::swap(data,other.data);
+    std::swap(size, other.size);
+    std::swap(capacity, other.capacity);
+    std::swap(data, other.data);
 }
 
 bool Dynamic_array::operator==(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    if (size != other.size)
     {
-        data1+=std::to_string(data[i]);
+        return false;
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        if (data[i] != other.data[i])
+        {
+            return false;
+        }
     }
-    return data1==data2;
+    return true;
+}
+
+Dynamic_array& Dynamic_array::operator=(const Dynamic_array& other)
+{
+    resize(other.size);
+    std::copy(other.data, other.data + other.capacity, data);
+    return *this;
 }
 
 bool Dynamic_array::operator!=(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    std::string data1 = "";
+    std::string data2 = "";
+    for (int i = 0;i < size;i++)
     {
-        data1+=std::to_string(data[i]);
+        data1 += std::to_string(data[i]);
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < other.size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        data2 += std::to_string(other.data[i]);
     }
-    return data1!=data2;
+    return data1 != data2;
 }
 
 bool Dynamic_array::operator>=(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    std::string data1 = "";
+    std::string data2 = "";
+    for (int i = 0;i < size;i++)
     {
-        data1+=std::to_string(data[i]);
+        data1 += std::to_string(data[i]);
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < other.size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        data2 += std::to_string(other.data[i]);
     }
-    return data1>=data2;
+    return data1 >= data2;
 }
 
 bool Dynamic_array::operator<=(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    std::string data1 = "";
+    std::string data2 = "";
+    for (int i = 0;i < size;i++)
     {
-        data1+=std::to_string(data[i]);
+        data1 += std::to_string(data[i]);
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < other.size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        data2 += std::to_string(other.data[i]);
     }
-    return data1<=data2;
+    return data1 <= data2;
 }
 
 bool Dynamic_array::operator>(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    std::string data1 = "";
+    std::string data2 = "";
+    for (int i = 0;i < size;i++)
     {
-        data1+=std::to_string(data[i]);
+        data1 += std::to_string(data[i]);
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < other.size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        data2 += std::to_string(other.data[i]);
     }
-    return data1>data2;
+    return data1 > data2;
 }
 
 bool Dynamic_array::operator<(const Dynamic_array& other) const
 {
-    std::string data1="";
-    std::string data2="";
-    for (int i=0;i<size;i++)
+    std::string data1 = "";
+    std::string data2 = "";
+    for (int i = 0;i < size;i++)
     {
-        data1+=std::to_string(data[i]);
+        data1 += std::to_string(data[i]);
     }
-    for (int i=0;i<other.size;i++)
+    for (int i = 0;i < other.size;i++)
     {
-        data2+=std::to_string(other.data[i]);
+        data2 += std::to_string(other.data[i]);
     }
-    return data1<data2;
+    return data1 < data2;
 }
-
+std::ostream& operator<<(std::ostream &os,Dynamic_array &other)
+{
+    for (int i=0;i<other.Size();i++)
+    {
+        os<<other[i]<<" ";
+    }
+    return os;
+}
