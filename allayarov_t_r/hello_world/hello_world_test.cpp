c6dab1683575deb8dc0ebb189f27063a493267c3
iteamurr <iteamurr@gmail.com>
2022-05-20 20:18:51 +0300

    Minor changes

--- allayarov_t_r/hello_world/hello_world_test.cpp
+++ allayarov_t_r/hello_world/hello_world_test.cpp
@@ -1,7 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
 
 #include "hello_world.h"
+
+#include "doctest.h"
 
 TEST_CASE("[hello_world] - testing HelloWorld function")
 {
