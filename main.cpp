fb7028821b42aa0f222e0b01a64b23a0d9ec6c48
leffff <levnovitskiy@gmail.com>
2022-05-21 13:25:43 +0300

    Dyn array added

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include "vector.h"
+#include <csignal>
+#include <algorithm>
+#include <valarray>
+
+
+Vector::Vector(Vector &vector) {
+    data = vector.get_data();
+}
+
+Vector::Vector(double[] _data) {
+    data = _data;
+}
+
+Vector Vector::operator+(double value) {
+    for (int i = i; i < size; ++i){
+        data[i] += value;
+    }
+    return this;
+}
+
+Vector Vector::operator-(const double value) {
+    for (int i = i; i < size; ++i){
+        data[i] -= value;
+    }
+
+    return this;
+}
+
+Vector Vector::operator*(const double value)  {
+    for (int i = i; i < size; ++i){
+        data[i] *= value;
+    }
+    return this;
+}
+
+int Vector::operator*(const Vector vector)  {
+    if (size != vector.get_size()){
+        raise( "Vector sizes dont match");
+    }
+
+    double s = 0;
+    for (int i = 0; i < size; ++i){
+        s += data[i] * vector[i];
+    }
+    return s;
+}
+
+Vector Vector::operator=(const Vector &vector) const {
+    data = vector.get_data();
+    return this;
+}
+
+bool Vector::operator==(const Vector &vector) const {
+    return data == vector.get_data();
+}
+
+bool Vector::operator!=(const Vector &vector) const {
+    return data != vector.get_data();
+}
+
+double Vector::operator[](int index) {
+    return data[index];
+}
+
+void Vector::norm() {
+    double normalizer;
+    for (int i = 0; i < size; ++ i){
+        normalizer += (data[i] * data[i]);
+    }
+    normalizer = std::sqrt(normalizer);
+    for (int i = 0; i < size; ++ i){
+        data[i] /= normalizer;
+    }
+}
+
+
+int Vector::get_size(){
+    return size;
+}
+
+double[] Vector::get_data(){
+    return data;
+}
---
+++ vector.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <ostream>
+
+class Vector {
+public:
+    Vector() = default;
+
+    Vector(Vector &vector);
+
+    Vector(double[] _data);
+
+    ~Vector() = default;
+
+    Vector operator+(const double &value) const;
+
+    Vector operator-(const double &value) const;
+
+    Vector operator*(const double &value) const;
+
+    Vector operator*(const Vector &vector) const;
+
+    Vector operator=(const Vector &vector);
+
+    const bool operator==(const Vector &vector);
+
+    const bool operator!=(const Vector &vector);
+
+    const double operator[](int index);
+
+    void norm();
+
+    int get_size();
+
+    double[]
+
+    get_data();
+
+
+private:
+    int size;
+    double[]
+    data;
+}
