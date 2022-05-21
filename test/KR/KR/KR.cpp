712f6e1c8786f7564edca98b1411317552686c33
Andryxa <andron_03333@mail.ru>
2022-05-21 13:33:05 +0300

    Adding KR

---
+++ test/KR/KR/KR.cpp
@@ -0,0 +1,11 @@
+﻿#include "vector.h"
+
+TVector& TVector::operator+(const TVector& rhs) {
+
+}
+TVector& TVector::operator-(const TVector& rhs) {
+
+}// Я, блин, не помню, как реализовывать
+TVector& TVector::operator=(const TVector& rhs) {
+
+}
