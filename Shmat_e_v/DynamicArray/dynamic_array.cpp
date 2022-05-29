2e6fa4806b40e2d9da764c1a29731e473e856d88
ShmatEugene <shmat.evg@yandex.ru>
2022-05-29 11:25:52 +0300

    add dynamic array

---
+++ Shmat_e_v/DynamicArray/dynamic_array.cpp
@@ -0,0 +1,172 @@
+#pragma once
+
+#include "dynamic_array.h"
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <stdexcept>
+
+DynamicArray::DynamicArray(int64_t _size, int _value) : size(_size), capacity(_size) {
+  data = new int[size];
+  std::fill(data, data + size, _value);
+}
+
+DynamicArray::DynamicArray(const DynamicArray &rhs) : size(rhs.size), capacity(rhs.capacity) {
+  data = new int[size];
+  std::copy(rhs.data, rhs.data + size, data);
+}
+
+DynamicArray::DynamicArray(const std::initializer_list<int> &list) : size(list.size()), capacity(list.size()) {
+  data = new int[size];
+  std::copy(list.begin(), list.end(), data);
+}
+
+DynamicArray::~DynamicArray() {
+  delete[] data;
+}
+
+int *DynamicArray::begin() const {
+  return data;
+}
+
+int *DynamicArray::end() const {
+  return data + size;
+}
+
+DynamicArray &DynamicArray::operator=(const DynamicArray &rhs) {
+  size = rhs.size;
+  capacity = rhs.capacity;
+
+  std::copy(rhs.data, rhs.data + rhs.size, data);
+  return *this;
+}
+
+int64_t DynamicArray::Size() const {
+  return size;
+}
+
+int64_t DynamicArray::Capacity() const {
+  return capacity;
+}
+
+bool DynamicArray::Empty() const {
+  return size == 0;
+}
+
+int &DynamicArray::operator[](const int64_t index) {
+  if (index < 0 || index >= size) {
+    throw std::out_of_range("Index is out of range");
+  }
+  return *(data + index);
+}
+
+void DynamicArray::push_back(int push_value) {
+  if (size == capacity) {
+    if (capacity == 0) capacity++;
+    reallocate(size + 1);
+  }
+  data[size++] = push_value;
+}
+
+void DynamicArray::reallocate(int64_t new_size) {
+  int64_t new_capacity = capacity;
+  if (new_size > capacity) {
+    new_capacity = std::max(new_capacity * 2, new_size);
+  }
+
+  int *new_data = new int[new_capacity];
+  std::copy(begin(), end(), new_data);
+  delete[] data;
+  data = new_data;
+  
+  capacity = new_capacity;
+}
+
+void DynamicArray::pop_back() {
+  if (size == 0) {
+    throw std::length_error("Array is empty");
+  }
+  --size;
+}
+
+void DynamicArray::clear() {
+  size = 0;
+}
+
+void DynamicArray::erase(int64_t index) {
+  if (size == 0) {
+    throw std::length_error("Array is empty");
+  }
+  if (index < 0 || index >= size) {
+    throw std::length_error("Index is out of range");
+  }
+
+  std::copy(data + index + 1, data + size, data + index);
+  --size;
+}
+
+void DynamicArray::insert(int64_t index, int insert_value) {
+  if (index < 0 || index >= size) {
+    throw std::length_error("Index is out of range");
+  }
+  if (size == capacity) {
+    if (capacity == 0) capacity++;
+    reallocate(size + 1);
+  }
+
+  std::copy_backward(data + index, data + size, data + size + 1);
+  data[index] = insert_value;
+  size++;
+}
+
+void DynamicArray::resize(int64_t new_size, int value) {
+  if (new_size < 0) {
+    throw std::length_error("Size must not be negative");
+  }
+  if (new_size == 0) {
+    size = 0;
+    return;
+  }
+  reallocate(new_size);
+  if (new_size > size) {
+    std::fill(begin() + ((size <= new_size) ? size : 0), begin() + new_size, value);
+  }
+  size = new_size;
+}
+
+void DynamicArray::assign(int64_t new_size, int value) {
+  if (new_size < 0) {
+    throw std::length_error("Size must not be negative");
+  }
+  if (new_size == 0) {
+    size = 0;
+    return;
+  }
+  reallocate(new_size);
+  std::fill(begin(), begin() + new_size, value);
+  size = new_size;
+}
+
+void DynamicArray::swap(DynamicArray &other) {
+  std::swap(size, other.size);
+  std::swap(capacity, other.capacity);
+  std::swap(data, other.data);
+}
+
+int &DynamicArray::at(int64_t index) const {
+  if (index < 0 || index >= size) {
+    throw std::out_of_range("Index is out of range");
+  }
+  return *(data + index);
+}
+
+bool DynamicArray::operator==(const DynamicArray &other) const {
+  return std::equal<int *, int *>(data, data + size, other.data);
+}
+
+bool DynamicArray::operator!=(const DynamicArray &other) const {
+  return !(*this == other);
+}
+
+
+
---
+++ Shmat_e_v/DynamicArray/test.cpp
@@ -0,0 +1,166 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <cstdint>
+#include "doctest.h"
+#include "dynamic_array.h"
+
+
+TEST_CASE("Dynamic array initialisation") {
+    DynamicArray a;
+    CHECK(a.Size() == 0);
+    CHECK(a.begin() == a.GetData());
+
+    DynamicArray b(5, 5);
+    CHECK(b.Size());
+    CHECK(b.begin() == b.GetData());
+    for (auto v: b) {
+        CHECK(v == 5);
+    }
+
+
+    DynamicArray c(b);
+    CHECK(c.Size());
+    CHECK(c.begin() == c.GetData());
+    for (int64_t i = 0; i < c.Size(); ++i) {
+        CHECK(c[i] == b[i]);
+    }
+
+    CHECK(c.end() == c.GetData() + c.Size());
+    CHECK(c.end() != c.GetData() + 0);
+
+    DynamicArray d = {5, 5, 5, 5, 5};
+    CHECK(d.Size() == 5);
+    CHECK(d.begin() == d.GetData());
+    for (int64_t i = 0; i < b.Size(); ++i) {
+        CHECK(d[i] == b[i]);
+    }
+}
+
+TEST_CASE("Testing push_back() and pop_back()") {
+    DynamicArray a;
+
+    a.push_back(1);
+    CHECK(a.Size() == 1);
+    CHECK(a[0] == 1);
+    a.push_back(2);
+    CHECK(a.Size() == 2);
+    CHECK(a[0] == 1);
+    CHECK(a[1] == 2);
+
+    for (int64_t i = 2; i <= 150; ++i) {
+      a.push_back(i);
+      CHECK(a.Size() == i + 1);
+    };
+
+    for (int64_t i = 2; i <= 150; ++i) {
+      CHECK(a[i] == i);
+    };
+
+    DynamicArray b = {1, 2, 3};
+    b.pop_back();
+    CHECK(b.Size() == 2);
+    CHECK(b[0] == 1);
+    CHECK(b[1] == 2);
+    CHECK_THROWS_WITH(b[2], "Index is out of range");
+    b.pop_back();
+    CHECK(b.Size() == 1);
+    b.pop_back();
+    CHECK(b.Size() == 0);
+    CHECK_THROWS_WITH(b.pop_back(), "Array is empty");
+}
+
+TEST_CASE("Testing erase()") {
+    DynamicArray a = {1, 2, 3, 4, 5};
+    DynamicArray after = {2, 3, 4, 5};
+
+    CHECK(a.Size() == 5);
+    a.erase(0);
+    CHECK_THROWS_MESSAGE(a.erase(-1), "Index is out of range");
+    CHECK_THROWS_MESSAGE(a.erase(10), "Index is out of range");
+    CHECK(a.Size() == 4);
+    for (int64_t i = 0; i < a.Size(); ++i) CHECK(a[i] == after[i]);
+
+    DynamicArray b;
+    CHECK_THROWS_MESSAGE(b.erase(1), "Array is empty");
+
+}
+
+TEST_CASE("Testing resize()") {
+
+    DynamicArray c = {1, 2, 3};
+    c.resize(5);
+    CHECK(c == DynamicArray({1, 2, 3, 0, 0}));
+    c.resize(2);
+    CHECK(c == DynamicArray({1, 2}));
+    c.resize(6, 4);
+    CHECK(c == DynamicArray({1, 2, 4, 4, 4, 4}));
+    c.resize(0);
+    CHECK(c.Size() == 0);
+
+    CHECK_THROWS_MESSAGE(c.resize(-3, 5), "Size must be positive");
+
+}
+
+TEST_CASE("Testing assign()") {
+    DynamicArray b(5, 1);
+    DynamicArray c(6, 2);
+
+    b.assign(4, 2);
+    for (int64_t i = 0; i < b.Size() - 1; ++i) CHECK(b[i] == c[i]);
+
+    CHECK_THROWS_WITH(b.assign(-3, 5), "Size must not be negative");
+}
+
+TEST_CASE("Testing insert()") {
+    DynamicArray a = {1, 2, 3, 4};
+    DynamicArray b = {1, 2, 5, 3, 4};
+    CHECK(a.Size() == 4);
+    CHECK(b.Size() == 5);
+
+    CHECK_THROWS_WITH(a.insert(-2, 5), "Index is out of range");
+
+    a.insert(2, 5);
+    CHECK(a.Size() == 5);
+    for (int64_t i = 0; i < a.Size(); ++i) CHECK(a[i] == b[i]);
+}
+
+TEST_CASE("Testing swap()") {
+    DynamicArray a = {1, 2, 3, 4};
+    DynamicArray b = {5, 6, 7, 8, 9};
+    DynamicArray before = {5, 6, 7, 8, 9};
+
+    a.swap(b);
+    CHECK(a.Size() == before.Size());
+    CHECK(a.Capacity() == before.Capacity());
+    CHECK(a == DynamicArray({5, 6, 7, 8, 9}));
+    CHECK(b == DynamicArray({1, 2, 3, 4}));
+}
+
+TEST_CASE("Testing Empty()") {
+    DynamicArray a;
+    CHECK(a.Empty());
+
+    a.push_back(1);
+    CHECK_FALSE(a.Empty());
+
+    a.pop_back();
+    CHECK(a.Empty());
+}
+
+TEST_CASE("Testing at()") {
+    DynamicArray a;
+    CHECK_THROWS_WITH(a.at(1), "Index is out of range");
+
+    a.push_back(1);
+    CHECK(a.at(0) == 1);
+}
+
+TEST_CASE("Testing operator =") {
+    DynamicArray a = {1, 2, 3};
+    DynamicArray b;
+
+    b = a;
+    CHECK(b.Size() == a.Size());
+    CHECK(b.Capacity() == a.Capacity());
+    for (int64_t i = 0; i < b.Size(); i++) CHECK(b[i] == a[i]);
+}
