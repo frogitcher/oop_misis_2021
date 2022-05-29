7967c3a07212d2a47f23a747411284c960fbd002
EgorTarasov <tarasov.egor.yandex@gmail.com>
2022-05-29 14:39:55 +0300

    minor changes to tests

--- tarasov_e_v/stack/test.cpp
+++ tarasov_e_v/stack/test.cpp
@@ -11,13 +11,7 @@ TEST_CASE_TEMPLATE("Testing constructors", T, int, double, float, long long,
   stack<T> a;
   CHECK(a.Size() == 0);
   CHECK(a.get_head() == nullptr);
-  // CHECK(Get_Tail(a) == nullptr);
 
   stack<T> b(a);
   CHECK(b.Size() == a.Size());
-  //    CHECK(b == a);
-  //
-  //    Stack<T> c(std::initializer_list<T>{ (T)6.4f, (T)-87.001, 3, 0 });
-  //    CHECK(c != b);
-  //    CHECK_FALSE(c == a);
 }
