9576b4bceb0acbb7bfdcc3a31026107c15a34f75
unknown <damir.ten.96@gmail.com>
2022-05-31 20:03:33 +0300

    stack

---
+++ Ten_d_s/stack/Stack.h
@@ -0,0 +1,166 @@
+﻿#pragma once
+#include <stdexcept>
+#include <initializer_list>
+
+template <typename T>
+class Stack {
+
+public:
+    struct Node {
+        T value;
+        Node* next;
+    };
+    Stack() = default;
+    Stack(const Stack<T>& minecraft) {
+        Copy(minecraft);
+    }
+    Stack(Stack<T>&& minecraft) {
+        size = minecraft.size;
+        head = minecraft.head;
+        tail = minecraft.tail;
+        minecraft.size = 0;
+        minecraft.head = nullptr;
+        minecraft.tail = nullptr;
+    }
+    Stack(const std::initializer_list<T>& list) {
+        for (auto& v : list) {
+            Push(v);
+        }
+    }
+    ~Stack(){
+        while (size > 0)
+        {
+            Pop();
+        }
+    }
+    bool Empty() const {
+        return size == 0;
+    }
+    void Push(const T& value) {
+        head = new Node{ value, head };
+        if (tail == nullptr) {
+            tail = head;
+        }
+        ++size;
+    }
+    size_t Size() const {
+        return size;
+    }
+    void Pop() {
+        if (Empty()) {
+            throw std::length_error("Stack is empty!");
+        }
+        Node* q = head;
+        head = q->next;
+        delete q;
+        if (head == nullptr) {
+            tail == nullptr;
+        }
+        --size;
+    }
+    T& Get() const {
+        if (Empty()) throw std::length_error("Stack is empty!");
+        return head->value;
+    }
+    void Clear() {
+        while (size > 0) {
+            Pop();
+        }
+    }
+    void Swap(Stack<T>& minecraft)
+    {
+        Node* temp = first;
+        first = minecraft.first;
+        minecraft.first = temp;
+        temp = last;
+        last = minecraft.last;
+        minecraft.last = temp;
+        size_t s = size;
+        size = minecraft.size;
+        minecraft.size = s;
+    }
+    Stack<T>& operator=(const Stack<T>& other)
+    {
+        while (!Empty())
+        {
+            Pop();
+        }
+        first = new Node{ other.first->value, nullptr };
+        Node* current = other.first->next;
+        Node* previous = first;
+        while (current != nullptr)
+        {
+            previous->next = new Node{ current->value, nullptr };
+            previous = previous->next;
+            current = current->next;
+        }
+        last = previous;
+        size = other.size;
+        return *this;
+    }
+    bool operator==(const Stack<T> minecraft) const
+    {
+        if (size != minecraft.size)
+        {
+            return false;
+        }
+        Node* a = first;
+        Node* b = minecraft.first;
+        while (a != nullptr)
+        {
+            if (a->value != b->value)
+            {
+                return false;
+            }
+            a = a->next;
+            b = b->next;
+        }
+        return true;
+    }
+    bool operator!=(const Stack<T> minecraft) const
+    {
+        return !(*this == minecraft);
+    }
+
+    void Merge(Stack<T>& other)
+    {
+        last->next = other.first;
+        last = other.last;
+        size += other.size;
+        other.last = nullptr;
+        other.first = nullptr;
+        other.size = 0;
+    }
+private:
+    size_t size = 0;
+    Node* first = nullptr;
+    Node* last = nullptr;
+};
+/* 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣴⣆⣠⣤⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣻⣿⣯⣘⠹⣧⣤⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⢿⣿⣷⣾⣯⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠜⣿⡍⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠘⣿⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡟⠃⡄⠀⠘⢿⣆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣁⣋⣈ ⣤⣮⣿⣧⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀
+⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀
+⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀
+⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀
+⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀
+⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀
+⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀
+⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+*/
