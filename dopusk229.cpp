bd7279f82fbc1a8bf5e4d1260b02e720b77cfc82
zokost <kostya.zorin.3213@mail.ru>
2022-06-04 13:15:33 +0300

    KR114 3

---
+++ dopusk229.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+#include <stdexcept>
+
+using namespace std;
+
+class Hyst
+{
+public:
+        Hyst()=default;
+        Hyst(initializer_list<double> list);
+        Hyst(double value, int64_t length);
+        ~Hyst();
+        void push_back(double value);
+        void vychest(double value,int64_t i);
+        void dobavit(double value,int64_t i);
+        double Get(int64_t i);
+        double Mediana() const;
+        int Size() const;
+        void Gyst();
+        bool operator==(const Hyst& other) const;
+        bool operator!=(const Hyst& other) const;
+        operator+(const Hyst& other);
+        operator-(const Hyst& other);
+        operator* (double n);
+
+private:
+    int64_t size = 0;
+    std::vector<double> data;
+};
+Hyst::Hyst(initializer_list<double> list)
+{
+    size=list.size();
+    std:;copy(list.begin(),list.end(),data);
+}
+Hyst::Hyst(double value, int64_t length)
+{
+    size=length;
+    for (int i=0;i<size;i++)
+    {
+        data.push_back(value);
+    }
+}
+Hyst::~Hyst()
+{
+    for (int i=0;i<size;i++)
+    {
+        data.clear();
+    }
+}
+void Hyst::push_back(double value)
+{
+    data.push_back(value);
+    size++;
+}
+void Hyst::vychest(double value,int64_t i)
+{
+    data[i]=data[i]-value;
+}
+void Hyst::dobavit(double value,int64_t i)
+{
+    data[i]=data[i]+value;
+}
+double Hyst::Get(int64_t i)
+{
+    if (size==0)
+    {
+        throw std::length_error("NO");
+    }
+    return data[i];
+}
+double Hyst::Mediana() const
+{
+    double m;
+    for (int64_t i=0;i<size;i++)
+    {
+        m+=data[i];
+    }
+    return (m/size);
+}
+void Hyst::Gyst()
+{
+    if (size==0)
+    {
+        throw std::length_error("NO");
+    }
+    for (int i=0;i<size;i++)
+    {
+        std::cout<<"value["<<i<<"]="<<data[i]<<endl;
+    }
+}
+bool Hyst::operator==(const Hyst& other) const
+{
+    return data==other.data;
+}
+bool Hyst::operator!=(const Hyst& other) const
+{
+    return data!=other.data;
+}
+int Hyst::Size() const
+{
+    return size;
+}
+
+
+
